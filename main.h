8e9ecf2c5996a50d78de292c3e4a299b1336c859
abelmesfin <abelmesfin555@gmail.com>
2022-07-11 01:02:20 +0300

    updated

--- main.h
+++ main.h
@@ -1,5 +1,16 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
+#include <stdarg.h>
+
+struct convert{
+        char *str;
+        int (*fun)(va_list);
+};
+
+int _putchar(char c);
+int p_char(va_list ap);
+int p_string(va_list ap);
+int _switch(const char *format, struct convert f_list[], va_list ap);
 int _printf(const char *format, ...);
 #endif
