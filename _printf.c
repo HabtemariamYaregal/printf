b202e43912d71c3f8d0567ad2cd7b574204073c3
abelmesfin <abelmesfin555@gmail.com>
2022-07-11 01:00:01 +0300

    commited

--- _printf.c
+++ _printf.c
@@ -10,34 +10,72 @@
  */
 int _printf(const char *format, ...)
 {
+	int print_len;
+	struct convert f_list[] = {
+		{"c", p_char},
+		{"s", p_string},
+		//{"%", p_percent},
+		{NULL, NULL}
+	};
 	va_list ap;
 
-	unsigned int j = 0, i = 0, k = 0;
-	char *str;
+	if (format == NULL)
+		return (-1);
 
 	va_start(ap, format);
-	while (format && format[j] != '\0')
-	{
-		while (format[j] != '%')
-		{
-			printf("%c", format[j]);
-			j++;
-			i++;	}
-		j++;
-		switch (format[j])
-		{ case 'c':
-			printf("%c", va_arg(ap, int));
-			i++;
-			break;
-		case's':
-			str = va_arg(ap, char*);
-			printf("%s", str);
-			while (str[k])
-			{	k++;
-				i++;	}
-			break;	}
-		j++;
-	}
+	/*Calling _switch  function*/
+	print_len = _switch(format, f_list, ap);
 	va_end(ap);
-	return (i);
+	return (print_len);
 }
+int _switch(const char *format, struct convert f_list[], va_list ap)
+{
+	int i, j, prt;
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			for (j = 0; f_list[j].str != NULL; j++)
+			{
+				if (format[i + 1] == f_list[j].str[0])
+				{
+					prt += f_list[j].fun(ap);
+					break;
+				}
+			}
+			if (f_list[j].str == NULL)
+			{
+				_putchar(format[i]);
+			}
+			i++;
+		}
+		else
+			prt += _putchar(format[i]);
+	}
+	return (prt);
+}
+int p_string(va_list ap)
+{
+	char c;
+	char *strg;
+	int i = 0, len = 0;
+
+	strg = va_arg(ap, char*);
+	while (strg[i] != '\0')
+	{
+		len += _putchar(strg[i]);
+		i++;
+	}
+	return (len);
+}
+int p_char(va_list ap)
+{
+	char c;
+	int len = 0;
+
+	c = va_arg(ap, int);
+	len += _putchar(c);
+	return (len);
+}
+
+
