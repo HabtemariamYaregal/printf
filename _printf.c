d2898824c1d59a14c90a6f988c63c5aeb1ff2331
HabtemariamYaregal <habminilik@gmail.com>
2022-07-11 18:24:54 -0700

    updating

--- _printf.c
+++ _printf.c
@@ -21,9 +21,9 @@ struct convert f_list[] = {
 {"s", p_string},
 {"%", p_percent},
 {"o", p_octal},
-/**{"u", p_decimal},
+{"u", p_decimal},
 {"x", p_hexd},
-{"X", p_heXd},*/
+{"X", p_heXd},
 {NULL, NULL}
 };
 va_list ap;
