ad3bd61f690765a3fc97a8063ec780c25b806049
HabtemariamYaregal <habminilik@gmail.com>
2022-07-11 17:54:17 -0700

    updating

---
+++ p_char_string.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+#include "main.h"
+
+/**
+ * p_string - print spesified taypes of data.
+ * @ap: contain data format of individual argument.
+ *
+ * Return: Number of charactors printed.
+ */
+
+int p_string(va_list ap)
+{
+char *strg;
+int i = 0, len = 0;
+strg = va_arg(ap, char*);
+while (strg[i] != '\0')
+{
+len += _putchar(strg[i]);
+i++;
+}
+return (len);
+}
+/**
+ * p_char - print spesified taypes of data.
+ * @ap: contain data format of individual argument.
+ *
+ * Return: Number of charactors printed.
+ */
+
+int p_char(va_list ap)
+{
+char c;
+int len = 0;
+c = va_arg(ap, int);
+len += _putchar(c);
+return (len);
+}
