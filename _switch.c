ec3698c5f9ecdb1cdb98db5f28cf7d3785902f3a
HabtemariamYaregal <habminilik@gmail.com>
2022-07-12 19:28:45 -0700

    editing format

--- _switch.c
+++ _switch.c
@@ -13,38 +13,37 @@
 
 int _switch(const char *format, conv f_list[], va_list ap)
 {
-	int i, j, prt, print_no;
-
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] == '%')
-		{
-			for (j = 0; f_list[j].str != NULL; j++)
-			{
-				if (format[i + 1] == f_list[j].str[0])
-				{
-					prt = f_list[j].fun(ap);
-					if (prt == -1)
-						return (-1);
-					print_no += prt;
-					break;
-				}
-			}
-			if (f_list[j].str == NULL && format[j + 1] != ' ')
-			{
-				if (format[i + 1] != '\0')
-				{
-					_putchar(format[i]);
-					_putchar(format[i + 1]);
-					print_no += 2;
-				}
-				else
-					return (-1);
-			}
-			i++;
-		}
-		else
-			print_no += _putchar(format[i]);
-	}
-	return (print_no);
+int i, j, prt, print_no;
+for (i = 0; format[i] != '\0'; i++)
+{
+if (format[i] == '%')
+{
+for (j = 0; f_list[j].str != NULL; j++)
+{
+if (format[i + 1] == f_list[j].str[0])
+{
+prt = f_list[j].fun(ap);
+if (prt == -1)
+return (-1);
+print_no += prt;
+break;
+}
+}
+if (f_list[j].str == NULL && format[j + 1] != ' ')
+{
+if (format[i + 1] != '\0')
+{
+_putchar(format[i]);
+_putchar(format[i + 1]);
+print_no += 2;
+}
+else
+return (-1);
+}
+i++;
+}
+else
+print_no += _putchar(format[i]);
+}
+return (print_no);
 }
