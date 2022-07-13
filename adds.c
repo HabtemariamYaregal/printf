90dcd419bdec9d74ca993b1bfd5f41c4c8e232f2
HabtemariamYaregal <habminilik@gmail.com>
2022-07-12 19:43:49 -0700

    editing format

--- adds.c
+++ adds.c
@@ -9,12 +9,11 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-		dest[i] = src[i];
-	dest[i] = '\0';
-	return (dest);
+unsigned int i;
+for (i = 0; i < n; i++)
+dest[i] = src[i];
+dest[i] = '\0';
+return (dest);
 }
 /**
  * rev_string - reverses a string in place
@@ -24,24 +23,21 @@ char *_memcpy(char *dest, char *src, unsigned int n)
  */
 char *rev_string(char *s)
 {
-	int len;
-	int head;
-	char tmp;
-	char *dest;
-
-	for (len = 0; s[len] != '\0'; len++)
-	{}
-
-	dest = malloc(sizeof(char) * len + 1);
-	if (dest == NULL)
-		return (NULL);
-
-	_memcpy(dest, s, len);
-	for (head = 0; head < len; head++, len--)
-	{
-		tmp = dest[len - 1];
-		dest[len - 1] = dest[head];
-		dest[head] = tmp;
-	}
-	return (dest);
+int len;
+int head;
+char tmp;
+char *dest;
+for (len = 0; s[len] != '\0'; len++)
+{}
+dest = malloc(sizeof(char) * len + 1);
+if (dest == NULL)
+return (NULL);
+_memcpy(dest, s, len);
+for (head = 0; head < len; head++, len--)
+{
+tmp = dest[len - 1];
+dest[len - 1] = dest[head];
+dest[head] = tmp;
+}
+return (dest);
 }
