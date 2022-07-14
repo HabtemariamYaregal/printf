6c08b7ef52cb860f4a9a1f00b0b038362c1c3d60
Abelmafi <105563023+Abelmafi@users.noreply.github.com>
2022-07-15 01:03:01 +0300

    Update print_adress.c

--- print_adress.c
+++ print_adress.c
@@ -1,35 +1,35 @@
 #include "main.h"
 /**
  * p_pointer - print spesified taypes of data.
- * @pb: contain data fotrmat of individual argument.
+ * @ptp: contain data fotrmat of individual argument.
  *
  * Return: Number of charactors printed.
  */
-int p_pointer(va_list pb, flag *f)
+int p_pointer(va_list ptp, flag *f)
 {
 	static const char Representation[] = "0123456789ABCDEF";
-	unsigned long int num, temp;
+	unsigned long int nuum, teemp;
 	char *hexx;
 	int i = 0, j = 0, plen = 0;
 	(void)f;
 
-	num = va_arg(pb, unsigned long int);
-	if (!num)
+	nuum = va_arg(ptp, unsigned long int);
+	if (!nuum)
 		return (_puts("(nil)"));
-	temp = num;
-	while (temp != 0)
+	teemp = nuum;
+	while (teemp != 0)
 	{
 		temp /= 16;
 		j++;
 	}
 	hexx = (char *)malloc(sizeof(char) * j + 1);
-	while (num >= 16)
+	while (nuum >= 16)
 	{
-		hexx[i] = Representation[num % 16];
-		num /= 16;
+		hexx[i] = Representation[nuum % 16];
+		nuum /= 16;
 		i++;
 	}
-	hexx[i] = Representation[num];
+	hexx[i] = Representation[nuum];
 	plen += _puts("0x");
 	while (i >= 0)
 	{
