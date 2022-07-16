6cc90f4643cbd6cedbfa907d0ca95d47f144a0d3
abelmesfin <abelmesfin555@gmail.com>
2022-07-16 20:37:34 +0300

    bug fix

--- adds.c
+++ adds.c
@@ -26,8 +26,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 	
 	for (i = 0; i < n; i++)
+	{
 		dest[i] = src[i];
-		dest[i] = '\0';
+	}
+	dest[i] = '\0';
 	return (dest);
 }
 /**
